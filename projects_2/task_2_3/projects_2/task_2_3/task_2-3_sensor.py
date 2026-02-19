38f92b3f90dabb8fa4f6b0e8b087b0e678191e82
bogdanoleynik25-byte <bogdanoleynik25@gmail.com>
2026-02-19 16:32:00 +0300

    Delete projects_2 directory

--- projects_2/task_2_3/projects_2/task_2_3/task_2-3_sensor.py
+++
@@ -1,5 +0,0 @@
-name = input('Введите имя оператора: ')
-pressure  = input('Введите текующее значение давления(Па): ')
-with open('sensor_log.txt', 'w', encoding = 'utf-8') as card:
-    card.write(f'{name} \t {pressure} Па')
-print('Данные успешно сохранены в sensor_log.txt')
--- projects_2/task_2_3/task_2-3_recipe.py
+++
@@ -1,6 +0,0 @@
-name = input('Введите название питательной среды: ')
-conc = input('Введите концентрацию агара(%): ')
-temp = input('Введите температуру стерилизации(ºС): ')
-with open('recipe.txt', 'w', encoding = 'utf-8') as card:
-    card.write(f'{name}\nКонцентрация:\t{conc}\nТемпература стерилизации:\t{temp}')
-print('Файл "recipe.txt" успешно сформирован!')
